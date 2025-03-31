dd4f5b1ea2e3206c975aa4be2773f3425282135c
heramb8962 <150731448+heramb8962@users.noreply.github.com>
2025-03-31 23:28:15 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config["SESSION_TYPE"] = "filesystem"
-    app.config["SECRET_KEY"] = "your_secret_key"
+    app.config["SECRET_KEY"] = "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"
     app.config["SESSION_PERMANENT"] = False
     app.config["SESSION_FILE_DIR"] = "./flask_session"
 
